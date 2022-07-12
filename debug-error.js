b3d51895537b6a8906ccc868f40194001a89c3ee
Lucas <lucasviniciuslimadepaula@gmail.com>
2022-07-11 23:06:16 -0300

    conclusão da atividade de tratamentos de erros

---
+++ debug-error.js
@@ -0,0 +1,44 @@
+// Atividade: validação de erros por tipo
+// O objetivo é que a função receba um array e retorne ele caso o seu tamanho corresponda ao número enviado como parâmetro na função. Caso contrário, um erro será lançado.
+
+// Crie uma função que recebe um array e um número
+// Realize as seguintes validações
+// Se os parâmetros não forem enviados, lance um erro do tipo ReferenceError
+// Se o array não for do tipo 'object', lance um erro do tipo TypeError
+// Se o número não for do tipo 'number', lance um erro do tipo TypeError
+// Se o tamanho do array for diferente do número enviado como parâmetro, lance um erro do tipo RangeError
+// Utilize a declaração try...catch
+// Filtre as chamadas de catch por cada tipo de erro utilizando o operador instanceof
+
+function validaArr(arr, numero){
+    try {
+        if(!arr && !numero) throw new ReferenceError("Envie os paramentros");
+
+    if(typeof arr !== "object") throw new TypeError("O array precisa ser do tipo object");
+
+    if(typeof numero !== "number") throw new TypeError("O numero precisa ser do tipo number");
+
+    if(arr.length !== numero) throw new RangeError("Tamanho Invalido");
+
+    return arr;
+        
+    } catch (e) {
+        if( e instanceof ReferenceError){
+            console.log("Este erro é um ReferenceError")
+            console.log(e.message)
+        }else if(e instanceof TypeError){
+            console.log("Este erro é um TypeError")
+            console.log(e.message)
+        }else if(e instanceof RangeError){
+            console.log("Este erro é um RangeError")
+            console.log(e.message)
+        }else{
+            console.log("Tipo de erro não esperado: " + e)
+        }
+
+
+    }
+
+}
+
+console.log(validaArr([1,4,5], 4))
